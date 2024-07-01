b73ad8eb48e7a49c5675aa692583730d23278473
Adam Plesnik <adam.plesnik@expersoft.com>
2024-07-02 00:14:31 +0200

    Add control buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Maximize2, Minimize2 } from 'lucide-react'
+import { Minus, Plus, RotateCcw } from 'lucide-react'
 import { useState } from 'react'
-import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch'
+import { TransformComponent, TransformWrapper, useControls } from 'react-zoom-pan-pinch'
 import Button from './components/Button'
 import Svg from './components/Svg'
 import WorkTable from './components/furniture/WorkTable'
@@ -11,46 +11,61 @@ import { getTableId } from './utils/getTableId'
 import plan from '/plan.svg'
 
 function App() {
-  const [fit, setFit] = useState(false)
   const [sidebarTableId, setSidebarTableId] = useState('')
+
+  const Controls = () => {
+    const { zoomIn, zoomOut, resetTransform } = useControls()
+
+    return (
+      <MenuBar>
+        <Button onClick={() => zoomIn()}>
+          <Plus />
+        </Button>
+        <Button onClick={() => zoomOut()}>
+          <Minus />
+        </Button>
+        <Button onClick={() => resetTransform()}>
+          <RotateCcw />
+        </Button>
+      </MenuBar>
+    )
+  }
 
   return (
     <>
-      <MenuBar>
-        <Button onClick={() => setFit(!fit)}>
-          <Maximize2 className={!fit ? 'hidden' : 'block'} />
-          <Minimize2 className={fit ? 'hidden' : 'block'} />
-        </Button>
-      </MenuBar>
       <div className="flex h-screen overflow-hidden">
         <TransformWrapper
           pinch={{ disabled: false }}
-          panning={{ wheelPanning: true, disabled: false }}
-          wheel={{ touchPadDisabled: true }}
-          minScale={0.1}
+          panning={{ wheelPanning: true, disabled: false, allowLeftClickPan: true }}
+          initialScale={0.4}
+          centerOnInit={true}
+          minScale={0.2}
           maxScale={1}
         >
-          <TransformComponent wrapperClass="!h-screen">
-            <div className="relative m-8">
-              {tables.map((t, i) => (
-                <WorkTable
-                  key={i}
-                  name={t.name}
-                  group={t.group}
-                  rotation={t.rotation}
-                  x={t.x}
-                  y={t.y}
-                  available={t.available}
-                  features={t.features}
-                  onClick={(e) => {
-                    e.preventDefault()
-                    setSidebarTableId(getTableId(t.name, t.group))
-                  }}
-                />
-              ))}
-              <Svg className={'bg-white' + (fit ? '' : ' max-w-fit')} source={plan} />
-            </div>
-          </TransformComponent>
+          <>
+            <Controls />
+            <TransformComponent wrapperClass="!h-screen">
+              <div className="relative m-8">
+                {tables.map((t, i) => (
+                  <WorkTable
+                    key={i}
+                    name={t.name}
+                    group={t.group}
+                    rotation={t.rotation}
+                    x={t.x}
+                    y={t.y}
+                    available={t.available}
+                    features={t.features}
+                    onClick={(e) => {
+                      e.preventDefault()
+                      setSidebarTableId(getTableId(t.name, t.group))
+                    }}
+                  />
+                ))}
+                <Svg className={'max-w-fit bg-white'} source={plan} />
+              </div>
+            </TransformComponent>
+          </>
         </TransformWrapper>
         <Sidebar
           className={sidebarTableId ? 'block' : 'hidden'}
