eca8c2d0667b8004c0b9910bace98d9899721a79
Adam Plesnik <adam.plesnik@expersoft.com>
2024-07-03 11:27:23 +0200

    Add more tables, border to the sidebar

--- src/partials/Sidebar.tsx
+++ src/partials/Sidebar.tsx
@@ -16,8 +16,7 @@ const Sidebar = ({ tableId, className, closeSidebar }: SidebarProps) => {
   return (
     <div
       className={
-        'flex h-screen w-xs shrink-0 flex-col overflow-y-scroll shadow-2xl' +
-        addWithSpace(className)
+        'flex h-screen w-xs shrink-0 flex-col overflow-y-scroll border-l ' + addWithSpace(className)
       }
     >
       <div className="sticky top-0 z-10 bg-gradient-to-b from-white/80 from-60% to-transparent pt-2 px-2 pb-4 text-end">
