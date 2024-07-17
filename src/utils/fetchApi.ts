f67f0ea89fd9b140461ac54c701d48952534b2d2
Adam Plesnik <adam.plesnik@expersoft.com>
2024-07-17 09:05:01 +0200

    Update env variables

--- src/utils/fetchApi.ts
+++ src/utils/fetchApi.ts
@@ -9,10 +9,10 @@ type TableQueryType = {
 
 export const loadTables = async (): Promise<TableQueryType> => {
   const response = await fetch(
-    `${import.meta.env.VITE_REACT_APP_API_URL}/tables?populate[features][fields][0]=uuid&fields[0]=x&fields[1]=y&fields[2]=width&fields[3]=height&fields[4]=name&fields[5]=rotation&fields[6]=available&fields[7]=rounded&fields[8]=uuid&publicationState=live&locale[0]=en`,
+    `${import.meta.env.VITE_API_URL}/tables?populate[features][fields][0]=uuid&fields[0]=x&fields[1]=y&fields[2]=width&fields[3]=height&fields[4]=name&fields[5]=rotation&fields[6]=available&fields[7]=rounded&fields[8]=uuid&publicationState=live&locale[0]=en`,
     {
       headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_PRIVATE_READ_ONLY_API_ID}`,
+        Authorization: `Bearer ${import.meta.env.VITE_PRIVATE_READ_ONLY_API_ID}`,
       },
     }
   )
@@ -24,9 +24,9 @@ type PlanQueryType = {
 }
 
 export const loadPlan = async (id: number): Promise<PlanQueryType> => {
-  const response = await fetch(`${import.meta.env.VITE_REACT_APP_API_URL}/plans/${id}`, {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/plans/${id}`, {
     headers: {
-      Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_PRIVATE_READ_ONLY_API_ID}`,
+      Authorization: `Bearer ${import.meta.env.VITE_PRIVATE_READ_ONLY_API_ID}`,
     },
   })
   return response.json()
@@ -38,10 +38,10 @@ type GroupMarkerQueryType = {
 
 export const loadMarkers = async (): Promise<GroupMarkerQueryType> => {
   const response = await fetch(
-    `${import.meta.env.VITE_REACT_APP_API_URL}/group-markers?populate[group][fields][0]=name&fields[0]=x&fields[1]=y`,
+    `${import.meta.env.VITE_API_URL}/group-markers?populate[group][fields][0]=name&fields[0]=x&fields[1]=y`,
     {
       headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_PRIVATE_READ_ONLY_API_ID}`,
+        Authorization: `Bearer ${import.meta.env.VITE_PRIVATE_READ_ONLY_API_ID}`,
       },
     }
   )
@@ -53,9 +53,9 @@ type FeatureQueryType = {
 }
 
 export const loadFeatures = async (): Promise<FeatureQueryType> => {
-  const response = await fetch(`${import.meta.env.VITE_REACT_APP_API_URL}/features`, {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/features`, {
     headers: {
-      Authorization: `Bearer ${import.meta.env.VITE_REACT_APP_PRIVATE_READ_ONLY_API_ID}`,
+      Authorization: `Bearer ${import.meta.env.VITE_PRIVATE_READ_ONLY_API_ID}`,
     },
   })
   return response.json()
