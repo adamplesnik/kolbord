478dddb10ab73693aa48533da766b8b10d1c9d21
Adam Plesnik <adam.plesnik@expersoft.com>
2024-08-13 16:16:11 +0200

    Add logo

--- src/partials/MenuBar.tsx
+++ src/partials/MenuBar.tsx
@@ -1,8 +1,10 @@
 import { HTMLAttributes } from 'react'
+import Logo from '../components/Logo'
 
 const MenuBar = ({ children }: HTMLAttributes<HTMLDivElement>) => {
   return (
     <div className="fixed bottom-2 left-2 z-50 flex items-center gap-1 rounded-xl bg-white/90 p-2 shadow-xl">
+      <Logo />
       {children}
     </div>
   )
