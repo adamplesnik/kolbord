4ea13f0fe82dafe3790b50085845d45a3a686a2e
Adam Plesnik <adam.plesnik@expersoft.com>
2024-08-26 20:28:57 +0200

    Update styling, tooltips

--- src/components/place/PlaceBookingSlot.tsx
+++ src/components/place/PlaceBookingSlot.tsx
@@ -2,7 +2,13 @@ import { ArrowRight, Trash2 } from 'lucide-react'
 import { Tooltip } from 'react-tooltip'
 import { addWithSpace } from '../../utils/addWithSpace'
 
-const PlaceBookingSlot = ({ dateFrom, dateTo, isBooked, isBookedByMe }: PlaceBookingSlotProps) => {
+const PlaceBookingSlot = ({
+  bookedBy,
+  dateFrom,
+  dateTo,
+  isBooked,
+  isBookedByMe,
+}: PlaceBookingSlotProps) => {
   const humanDate = (date: Date) => {
     return date.toLocaleString([], { hour: '2-digit', minute: '2-digit' })
   }
@@ -14,14 +20,18 @@ const PlaceBookingSlot = ({ dateFrom, dateTo, isBooked, isBookedByMe }: PlaceBoo
       <div
         data-tooltip-id={tooltipId}
         className={
-          'relative flex flex-1 items-center justify-center gap-1 overflow-clip rounded-full border p-1 text-sm font-medium' +
+          'group relative flex flex-1 items-center justify-center gap-1 overflow-clip rounded-full border p-1 text-sm font-medium transition-colors' +
           addWithSpace(
             isBooked
               ? 'border-red-400 bg-pink-50'
               : 'cursor-pointer border-slate-300 hover:border-slate-900 hover:bg-slate-800 hover:text-white'
           ) +
-          addWithSpace(isBooked && !isBookedByMe ? 'opacity-40' : '') +
-          addWithSpace(isBookedByMe ? 'cursor-pointer hover:border-red-500 hover:bg-pink-100' : '')
+          addWithSpace(isBooked && !isBookedByMe && 'opacity-40') +
+          addWithSpace(
+            isBookedByMe
+              ? 'cursor-pointer hover:border-red-500 hover:bg-pink-100'
+              : 'cursor-not-allowed'
+          )
         }
       >
         {humanDate(dateFrom)}
@@ -32,11 +42,11 @@ const PlaceBookingSlot = ({ dateFrom, dateTo, isBooked, isBookedByMe }: PlaceBoo
       {isBooked && (
         <Tooltip id={tooltipId}>
           {isBookedByMe ? (
-            <>
-              <Trash2 /> Click to unbook.
-            </>
+            <span className="flex items-center gap-1 text-xs">
+              <Trash2 className="size-5" strokeWidth={1} /> Click to unbook.
+            </span>
           ) : (
-            <span className="text-xs">Mike Pence</span>
+            <span className="text-xs">{bookedBy}</span>
           )}
         </Tooltip>
       )}
@@ -45,6 +55,7 @@ const PlaceBookingSlot = ({ dateFrom, dateTo, isBooked, isBookedByMe }: PlaceBoo
 }
 
 type PlaceBookingSlotProps = {
+  bookedBy?: string
   dateFrom: Date
   dateTo: Date
   isBooked: boolean
