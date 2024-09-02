b72f34c84bf5c20f000e7ef72863c0235be271e4
Adam Plesnik <adam.plesnik@expersoft.com>
2024-09-02 21:16:04 +0200

    Tune link colors

--- src/components/basic/A.tsx
+++ src/components/basic/A.tsx
@@ -1,18 +1,24 @@
 import { Link, LinkProps } from 'react-router-dom'
 import { addWithSpace } from '../../utils/addWithSpace'
 
-const A = ({ to, target, children, className }: AProps) => {
+const A = ({ to, target, children, className, mono = false }: AProps) => {
   return (
     <Link
       to={to}
       target={target}
-      className={'cursor-pointer text-cyan-600 hover:text-cyan-800' + addWithSpace(className)}
+      className={
+        'cursor-pointer underline-offset-1 hover:text-sky-500 hover:underline' +
+        addWithSpace(mono ? 'text-slate-700' : 'text-sky-700') +
+        addWithSpace(className)
+      }
     >
       {children}
     </Link>
   )
 }
 
-type AProps = {} & LinkProps
+type AProps = {
+  mono?: boolean
+} & LinkProps
 
 export default A
