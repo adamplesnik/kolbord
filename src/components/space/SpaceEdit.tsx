ccb2572421e658dfec0b1be57acb4f181edfc9cd
Adam Plesnik <adam.plesnik@expersoft.com>
2024-09-09 22:45:45 +0200

    Feature edit v1

--- src/components/space/SpaceEdit.tsx
+++ src/components/space/SpaceEdit.tsx
@@ -1,24 +1,50 @@
 import { useForm } from '@tanstack/react-form'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { HTMLAttributes, useEffect } from 'react'
+import { HTMLAttributes, useEffect, useState } from 'react'
 import { getToken } from '../../auth/helpers'
 import { TableRecord } from '../../data/TableRecord'
 import { LATEST_PLACE_METADATA } from '../../utils/constants'
+import CheckboxWithLabel from '../basic/CheckboxWithLabel.tsx'
 import FetchStatus from '../basic/FetchStatus'
 import InputWithLabel from '../basic/InputWithLabel'
 import SpaceDelete from './SpaceDelete.tsx'
+import { useFeaturesQuery } from './loadFeatures.ts'
 
 const SpaceEdit = ({ table, planId, handleDelete }: SpaceEditProps) => {
   const updateTable = async (id: number, data: TableRecord): Promise<TableRecord> => {
+    const payload = {
+      name: data.attributes.name,
+      x: data.attributes.x,
+      y: data.attributes.y,
+      slots: data.attributes.slots,
+      features: currentFeatures,
+    }
     const response = await fetch(`${import.meta.env.VITE_API_URL}/tables/${id}`, {
       method: 'put',
       headers: {
         Authorization: `Bearer ${getToken()}`,
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ data: data.attributes }),
+      body: JSON.stringify({ data: payload }),
     })
     return response.json()
+  }
+
+  const { data: allFeatures } = useFeaturesQuery()
+  const [currentFeatures, setCurrentFeatures] = useState(
+    table?.attributes.features.data.map((d) => d.id)
+  )
+
+  useEffect(() => {
+    setCurrentFeatures(table?.attributes.features.data.map((d) => d.id))
+  }, [table])
+
+  const handleFeatureChange = (id: number) => {
+    setCurrentFeatures((prevFeatures) =>
+      prevFeatures.includes(id) ?
+        prevFeatures.filter((featureId) => featureId !== id)
+      : [...prevFeatures, id]
+    )
   }
 
   const { Field, handleSubmit, reset } = useForm<TableRecord>({
@@ -44,7 +70,9 @@ const SpaceEdit = ({ table, planId, handleDelete }: SpaceEditProps) => {
             },
           },
         },
-        features: table?.attributes.features,
+        features: {
+          data: [],
+        },
       },
     },
   })
@@ -155,19 +183,19 @@ const SpaceEdit = ({ table, planId, handleDelete }: SpaceEditProps) => {
               )}
             />
           </div>
+          <span className="text-sm font-bold">Features</span>
+          <div className="flex flex-col gap-0.5">
+            {allFeatures?.data.map((f) => (
+              <CheckboxWithLabel
+                key={`feature_checkbox${f.id}`}
+                checked={currentFeatures.includes(f.id)}
+                label={allFeatures?.data.find((a) => a.id === f.id)?.attributes.description}
+                onBlur={() => handleFeatureChange(f.id)}
+                onChange={() => handleFeatureChange(f.id)}
+              />
+            ))}
+          </div>
         </div>
-        <Field name="attributes.features.data" mode="array">
-          {(field) => {
-            return (
-              <div>
-                {field.state.value.map((value, i) => {
-                  console.log(field.state.value)
-                  return <>{/* <Field name={}></Field> */}</>
-                })}
-              </div>
-            )
-          }}
-        </Field>
       </form>
       <SpaceDelete id={table.id} handleDelete={handleDelete} />
     </>
