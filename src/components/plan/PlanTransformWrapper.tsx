1357fa6e57a5c3066c8705afb8943fc18b7efd34
Adam Plesnik <adam.plesnik@expersoft.com>
2024-09-11 21:37:16 +0200

    Remove plan controls, gradient

--- src/components/plan/PlanTransformWrapper.tsx
+++ src/components/plan/PlanTransformWrapper.tsx
@@ -1,8 +1,5 @@
-import { Fullscreen, ZoomIn, ZoomOut } from 'lucide-react'
-import { TransformComponent, TransformWrapper, useControls } from 'react-zoom-pan-pinch'
-import MenuBar from '../../partials/MenuBar'
+import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch'
 import { addWithSpace } from '../../utils/addWithSpace'
-import Button from '../basic/Button'
 import GroupMarkers from '../group/GroupMarkers'
 import Spaces from '../space/Spaces'
 import Plan from './Plan'
@@ -16,20 +13,6 @@ const PlanTransformWrapper = ({
   sidebarTableId,
   workingDate,
 }: PlanTransformWrapperProps) => {
-  const PlanControls = () => {
-    const { zoomIn, zoomOut, resetTransform } = useControls()
-
-    return (
-      <MenuBar position="top">
-        <div className="flex">
-          <Button onClick={() => zoomIn} Icon={ZoomIn}></Button>
-          <Button onClick={() => zoomOut} Icon={ZoomOut}></Button>
-          <Button onClick={() => resetTransform} Icon={Fullscreen}></Button>
-        </div>
-      </MenuBar>
-    )
-  }
-
   return (
     <TransformWrapper
       pinch={{ disabled: false }}
@@ -39,27 +22,24 @@ const PlanTransformWrapper = ({
       minScale={0.2}
       maxScale={1}
     >
-      <>
-        <PlanControls />
-        <TransformComponent wrapperClass="!h-screen !w-full bg-gradient-to-tr from-zinc-300 to-zinc-100 !p-2">
-          <div
-            className={
-              'relative m-8 rounded-3xl bg-white p-2 outline-[1.5rem] outline-white' +
-              addWithSpace(sidebarTableId > 0 || sidebarPlanEdit ? 'mr-[23rem]' : '')
-            }
-          >
-            <GroupMarkers planId={planId} />
-            <Spaces
-              sidebarTableId={sidebarTableId}
-              handlePlaceClick={handlePlaceClick}
-              planId={planId}
-              workingDate={workingDate}
-              listView={listView}
-            />
-            {planId > 0 && <Plan planId={planId} />}
-          </div>
-        </TransformComponent>
-      </>
+      <TransformComponent wrapperClass="!h-screen !w-full  !p-2">
+        <div
+          className={
+            'relative m-8 rounded-3xl bg-white p-2 outline-[1.5rem] outline-white' +
+            addWithSpace(sidebarTableId > 0 || sidebarPlanEdit ? 'mr-[23rem]' : '')
+          }
+        >
+          <GroupMarkers planId={planId} />
+          <Spaces
+            sidebarTableId={sidebarTableId}
+            handlePlaceClick={handlePlaceClick}
+            planId={planId}
+            workingDate={workingDate}
+            listView={listView}
+          />
+          {planId > 0 && <Plan planId={planId} />}
+        </div>
+      </TransformComponent>
     </TransformWrapper>
   )
 }
