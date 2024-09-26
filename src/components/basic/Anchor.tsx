a654811afe862bbeee3cb717af6fb43c61f02ddd
Adam Plesnik <adam.plesnik@gmail.com>
2024-09-26 10:04:50 +0200

    Add AuthFooter, tune Anchor

---
+++ src/components/basic/Anchor.tsx
@@ -0,0 +1,19 @@
+import { Link, LinkProps } from 'react-router-dom'
+import { addWithSpace } from '../../utils/addWithSpace'
+
+const Anchor = ({ to, target, children, className }: LinkProps) => {
+  return (
+    <Link
+      to={to}
+      target={target}
+      className={
+        'cursor-pointer underline-offset-1 hover:text-slate-950 hover:underline' +
+        addWithSpace(className)
+      }
+    >
+      {children}
+    </Link>
+  )
+}
+
+export default Anchor
