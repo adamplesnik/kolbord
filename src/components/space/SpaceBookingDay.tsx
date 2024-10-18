1387a26b32f26a518dbb0a6fc4cdca8eb80967d8
Adam Plesnik <adam.plesnik@gmail.com>
2024-10-18 10:00:57 +0200

    Test various booking options

--- src/components/space/SpaceBookingDay.tsx
+++ src/components/space/SpaceBookingDay.tsx
@@ -2,12 +2,15 @@ import { useAuth } from '@clerk/clerk-react'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import clsx from 'clsx'
+import { AnimatePresence, motion } from 'framer-motion'
+import { ArrowRight } from 'lucide-react'
 import qs from 'qs'
 import { useContext, useState } from 'react'
 import { SidebarContext, SidebarContextType } from '../../providers/SidebarContextProvider.tsx'
 import { ZoneContext, ZoneContextType } from '../../providers/ZoneContextProvider.tsx'
 import { BookingType } from '../../types/bookingType'
 import { humanTime } from '../../utils/human.ts'
+import Button from '../atoms/Button.tsx'
 import DateHeading from '../basic/DateHeading.tsx'
 import { generateTimeSlots } from './generateSlots'
 
@@ -101,33 +104,90 @@ const SpaceBookingDay = ({ date }: SpaceBookingDayProps) => {
 
   console.info(loadedSpaceBooking)
 
+  let firstActiveIndex = -1
+  let lastActiveIndex = -1
+
+  const timeSlots = (date: Date, hoursFrom: number | undefined, hoursTo: number | undefined) => {
+    if (hoursFrom && hoursTo) {
+      return generateTimeSlots(date, hoursFrom, hoursTo)
+    } else return []
+  }
+
+  timeSlots(date, zone?.hoursFrom, zone?.hoursTo).map((d, i) => {
+    const isActive = markActive(booking.from, booking.to, d)
+    if (isActive && firstActiveIndex === -1) {
+      firstActiveIndex = i
+    }
+
+    if (isActive) {
+      lastActiveIndex = i
+    }
+  })
+
   return (
     <div className="flex flex-col gap-3">
       <DateHeading date={date} />
-      {booking.from?.toLocaleTimeString()}, {booking.to?.toLocaleTimeString()}
-      <div className="grid grid-cols-4 gap-1">
-        {zone?.hoursFrom &&
-          zone?.hoursTo &&
-          generateTimeSlots(date, zone?.hoursFrom, zone?.hoursTo).map((d) => {
-            const isActive = markActive(booking.from, booking.to, d)
-            return (
-              <div
-                key={`slot_${d.getTime()}`}
-                className={clsx(
-                  'peer relative z-10 cursor-pointer gap-0.5 p-0.5 text-center',
-                  isActive ?
-                    'rounded-l bg-slate-600 text-white hover:bg-slate-500 [.bg-slate-600:nth-last-child(-n+1))]:rounded-r'
-                  : 'rounded bg-teal-100 hover:bg-emerald-100',
-                  '[+.bg-slate-600]:bg-red-100',
-                  isActive &&
-                    'after:absolute after:inset-0 after:-right-1 after:-z-10 after:bg-zinc-500 nth-[4]:after:right-0'
-                )}
-                onClick={() => setCurrentBookingTime(d)}
-              >
-                {humanTime(d)}
-              </div>
-            )
-          })}
+      <div className={clsx('flex items-center gap-1', booking.from ? '' : 'bg-zinc-100')}>
+        {booking.from ?
+          booking.from.toLocaleString([], { hour: '2-digit', minute: '2-digit' })
+        : 'nn'}
+        <ArrowRight className="size-4 text-zinc-600" />
+        {booking.to?.toLocaleString([], { hour: '2-digit', minute: '2-digit' })}
+        <Button>confirm</Button>
+      </div>
+      <div className="grid grid-cols-4 bg-gradient-to-bl from-cyan-200 to-fuchsia-300">
+        <AnimatePresence>
+          {zone?.hoursFrom &&
+            zone?.hoursTo &&
+            generateTimeSlots(date, zone?.hoursFrom, zone?.hoursTo).map((d, i) => {
+              const isActive = markActive(booking.from, booking.to, d)
+
+              console.log(Math.floor(firstActiveIndex / 4))
+              return (
+                <div
+                  className={clsx(
+                    'relative h-8 overflow-hidden',
+                    firstActiveIndex - 1 === i && 'rounded-br-lg'
+                  )}
+                  key={`slot_${d.getTime()}`}
+                >
+                  {isActive && firstActiveIndex === i && (
+                    <div className="absolute top-0 left-0 size-4 rounded-tl-full bg-red-500/50 shadow-[-10px_-15px_0_10px_#fff]"></div>
+                  )}
+                  {isActive && firstActiveIndex === i && lastActiveIndex < i + 4 && (
+                    <div className="absolute bottom-0 left-0 size-4 rounded-bl-full bg-transparent shadow-[-10px_15px_0_10px_#fff]"></div>
+                  )}
+                  {isActive && lastActiveIndex === i && (
+                    <div className="absolute right-0 bottom-0 size-4 rounded-br-full bg-transparent shadow-[10px_15px_0_10px_#fff]"></div>
+                  )}
+                  {isActive ?
+                    <motion.div
+                      initial={false}
+                      animate={{ x: 0, opacity: 1 }}
+                      exit={{ x: '100%', opacity: 0 }}
+                      transition={{ duration: 0.4 }}
+                      key={`slot_${d.getTime()}1`}
+                      className={clsx('p-1 text-xs inset-shadow-slate-500')}
+                      onClick={() => setCurrentBookingTime(d)}
+                    >
+                      i: {i}, f: {firstActiveIndex},{humanTime(d)}
+                    </motion.div>
+                  : <motion.div
+                      initial={false}
+                      animate={{ x: 0, opacity: 1 }}
+                      exit={{ x: '-10px', opacity: 0 }}
+                      transition={{ duration: 0.4 }}
+                      key={`slot_${d.getTime()}2`}
+                      className={clsx('bg-white p-1')}
+                      onClick={() => setCurrentBookingTime(d)}
+                    >
+                      {humanTime(d)}
+                    </motion.div>
+                  }
+                </div>
+              )
+            })}
+        </AnimatePresence>
       </div>
     </div>
   )
