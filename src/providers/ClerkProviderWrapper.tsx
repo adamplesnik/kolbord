db31c69df2ef28d9249592e7db31941948079ab2
Adam Plesnik <adam.plesnik@gmail.com>
2024-10-29 20:29:48 +0100

    Remove some Clerk overrides

--- src/providers/ClerkProviderWrapper.tsx
+++ src/providers/ClerkProviderWrapper.tsx
@@ -15,31 +15,20 @@ const ClerkProviderWrapper = ({ children }: HTMLAttributes<HTMLDivElement>) => {
       appearance={{
         layout: {
           socialButtonsVariant: 'blockButton',
-          unsafe_disableDevelopmentModeWarnings: true,
           logoPlacement: 'none',
         },
         variables: {
           fontSize: 'var(--font-size-sm)',
         },
         elements: {
-          avatarBox: '!rounded-md !size-8',
-          card: '!p-px !shadow-none !border-none !rounded-none !bg-transparent !text-current',
-          cardBox: '!shadow-none !w-full',
-          footer: '!pt-0 *:!border-none !rounded-full !w-fit',
-          footerAction: '!hidden',
+          badge: 'text-xs',
+          formFieldHintText: 'text-sm opacity-50',
           formButtonPrimary:
-            '!rounded !shadow-none after:hidden !text-base ![--accent:var(--color-zinc-800)] ![--accentHover:var(--color-zinc-700)] min-h-10',
-          formFieldInput: '!rounded !text-base !shadow-none !border-1',
-          formFieldLabel: '!text-xs',
+            '!shadow-none after:hidden ![--accent:var(--color-zinc-800)] ![--accentHover:var(--color-zinc-700)] min-h-10',
+          formFieldInput: '!rounded-md !shadow-none !border-1',
           headerTitle: '!text-xl text-left !font-semibold',
           headerSubtitle: '!hidden',
-          identityPreview: '!justify-start !pt-1',
-          organizationListPreviewButton: '!text-red-400',
-          organizationPreviewSecondaryIdentifier: 'pt-1 !text-zinc-500',
-          scrollBox: '!rounded-none',
-          userButtonBox: '!rounded-md !size-8',
-          userButtonTrigger: 'focus:!shadow-none',
-          userPreviewSecondaryIdentifier: 'pt-1 !text-zinc-500',
+          footer: 'text-xs',
         },
       }}
     >
